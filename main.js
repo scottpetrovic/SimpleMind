232dd6f17b8185d6898bb9181cf5f55bce287e21
Scott Petrovic <scottpetrovic@gmail.com>
2024-07-22 08:12:13 -0500

    Add in shortcut to turn on and off dev tools for debugging

--- main.js
+++ main.js
@@ -1,11 +1,17 @@
 // app - controls the application's event lifecycle
 // BrowserWindow - creates and manages the browser window
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow, globalShortcut } = require('electron')
+const { ipcRenderer } = require('electron');
 
 const createWindow = () => {
   const win = new BrowserWindow({
     width: 800,
-    height: 600
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+      // Note: For security reasons, you should keep contextIsolation enabled and use contextBridge for IPC
+      contextIsolation: false, 
+    }
   })
 
   win.loadFile('index.html')
@@ -14,6 +20,33 @@ const createWindow = () => {
 // hook when the application is ready to start doing work
 app.whenReady().then(() => {
   createWindow()
+
+
+
+  // Register a global shortcut to open DevTools
+  globalShortcut.register('CommandOrControl+Shift+I', () => {
+    const win = BrowserWindow.getFocusedWindow();
+    if (win) {
+      
+      if (win.webContents.isDevToolsOpened()) {     
+        win.webContents.closeDevTools();
+      } else {     
+        win.webContents.openDevTools();
+      }
+
+
+    }
+  });
+
+
+// Unregister all shortcuts when the application is quitting.
+app.on('will-quit', () => {
+  globalShortcut.unregisterAll();
+});
+
+
+
+
 
   // lifecycle hook specific to MacOS for opening window
   app.on('activate', () => {
